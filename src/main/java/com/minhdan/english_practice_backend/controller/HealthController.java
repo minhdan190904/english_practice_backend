da5b603b627dcb926269d80293ce86c2a5ff410e
admin <minhdan190904@gmail.com>
2026-05-18 09:12:40 +0700

    ok

---
+++ src/main/java/com/minhdan/english_practice_backend/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.minhdan.english_practice_backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/public")
+public class HealthController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("Server is running!");
+    }
+}
